afec488ad46dce91156ef810d319460ab0675f2f
Jack Peng <t-systems.peng@mercedes-benz.com>
2025-10-26 17:36:19 +0800

    1

--- setup-k8s/scripts/04-kubeadm-init.sh
+++ setup-k8s/scripts/04-kubeadm-init.sh
@@ -11,6 +11,7 @@ echo "$MASTER_NODES,127.0.0.1,$VIP,api.k8s.local,k8s-api.internal"
 
 if [ "${REGION}X" = "CNX" ]; then
     color_echo "Using China image repository for kubeadm init ..."
+    cat <<EOF    
     sudo kubeadm init \
       --node-name=$(hostname) \
       --image-repository registry.cn-hangzhou.aliyuncs.com/google_containers \
@@ -22,6 +23,18 @@ if [ "${REGION}X" = "CNX" ]; then
       --v 9 \
       --upload-certs \
       --ignore-preflight-errors=all
+EOF
+    sudo kubeadm init \
+      --node-name=$(hostname) \
+      --image-repository registry.cn-hangzhou.aliyuncs.com/google_containers \
+      --apiserver-advertise-address=$(ip r get 1.1.1.1 | awk '/src/{print $7}') \
+      --apiserver-cert-extra-sans "$MASTER_NODES,127.0.0.1,$VIP,api.k8s.local,k8s-api.internal" \
+      --service-cidr=$SERVICE_CIDR \
+      --pod-network-cidr=$POD_CIDR \
+      --cri-socket=unix:///var/run/containerd/containerd.sock `# unix:///run/cri-dockerd.sock` \
+      --v 9 \
+      --upload-certs \
+      --ignore-preflight-errors=all      
 else
     color_echo "Using default image repository for kubeadm init ..."
     sudo kubeadm init \
